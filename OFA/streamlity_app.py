5913ed75c025513898304ef29e8ef7219e3af501
Khaled Nabil <49573699+Khaled110@users.noreply.github.com>
2022-08-07 21:39:27 +0200

    Update streamlity_app.py

--- OFA/streamlity_app.py
+++ OFA/streamlity_app.py
@@ -19,7 +19,7 @@ import wget
 @st.cache(suppress_st_warning=True , allow_output_mutation=True)
 def load_model():
     
-    filename = wget.download('https://ofa-silicon.oss-us-west-1.aliyuncs.com/checkpoints/caption_large_best_clean.pt', out="checkpoints/caption.pt")
+    filename = wget.download('https://ofa-silicon.oss-us-west-1.aliyuncs.com/checkpoints/caption_large_best_clean.pt')
     
     # Register caption task
     tasks.register_task('caption',CaptionTask)
@@ -33,7 +33,7 @@ def load_model():
     # Load pretrained ckpt & config
     overrides={"bpe_dir":"utils/BPE", "eval_cider":False, "beam":5, "max_len_b":16, "no_repeat_ngram_size":3, "seed":7}
     models, cfg, task = checkpoint_utils.load_model_ensemble_and_task(
-            utils.split_paths('checkpoints/caption.pt'),
+            utils.split_paths('caption_large_best_clean.pt'),
             arg_overrides=overrides
         )
 
